c1c6feb29b759cd00fb36da479bbd6db4b5088e3
Renan Pallin <renanpallin@gmail.com>
2019-09-18 15:56:07 -0300

    Add PlaceRepository interface

---
+++ testes/backend-developer/solution/src/main/java/com/pallin/clickbus/repository/PlaceRepository.java
@@ -0,0 +1,7 @@
+package com.pallin.clickbus.repository;
+
+import com.pallin.clickbus.model.Place;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlaceRepository extends CrudRepository<Place, Long> {
+}
