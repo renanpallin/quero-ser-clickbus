eda2a151fa9e87e899990defb443c08f62d29737
Renan Pallin <renanpallin@gmail.com>
2019-09-18 15:17:46 -0300

    Initial project structure

---
+++ testes/backend-developer/solution/src/test/java/com/pallin/clickbus/ClickbusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pallin.clickbus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClickbusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
