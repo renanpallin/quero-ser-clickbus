d0d9fad1f58c23637e4df0cb2cb4c7e5e8a24547
Renan Pallin <renanpallin@gmail.com>
2019-09-18 16:01:24 -0300

    First test endpoint on PlaceController

--- testes/backend-developer/solution/src/main/java/com/pallin/clickbus/controller/PlaceController.java
+++ testes/backend-developer/solution/src/main/java/com/pallin/clickbus/controller/PlaceController.java
@@ -1,10 +1,21 @@
 package com.pallin.clickbus.controller;
 
+import com.pallin.clickbus.repository.PlaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/place")
 public class PlaceController {
+
+    @Autowired
+    private PlaceRepository repository;
+
+    @GetMapping()
+    public String index() {
+        return "Bora ser clickbuser";
+    }
 
 }
