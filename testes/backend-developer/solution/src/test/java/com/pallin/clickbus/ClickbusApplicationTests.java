a04bf954ed8de3a0344c83d22dc052110006826a
Renan Pallin <renanpallin@gmail.com>
2019-09-24 22:46:39 -0300

    Add testGetPlaceById

--- testes/backend-developer/solution/src/test/java/com/pallin/clickbus/ClickbusApplicationTests.java
+++ testes/backend-developer/solution/src/test/java/com/pallin/clickbus/ClickbusApplicationTests.java
@@ -1,16 +1,47 @@
 package com.pallin.clickbus;
 
+import com.pallin.clickbus.controller.PlaceController;
+import com.pallin.clickbus.model.Place;
+import com.pallin.clickbus.repository.PlaceRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class ClickbusApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    @InjectMocks
+    private PlaceController placeController;
+
+    @Mock
+    private PlaceRepository placeRepository;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
     }
 
+    /**
+     * Test getting an specific place by ID
+     */
+    @Test
+    public void testGetPlaceById() {
+        Place mockPlace = new Place();
+        mockPlace.setId(1L);
+
+        when(placeRepository.findById(1L)).thenReturn(Optional.of(mockPlace));
+        Place place = placeController.showById(mockPlace).getBody();
+        assertThat(place.getId(), is(1L));
+    }
 }
